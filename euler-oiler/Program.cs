5dea98323e1350c6a1c738beaf09a88a4133818f
cameron_rodgers <cameronrodgers96@gmail.com>
2022-04-09 16:03:38 +0100

    Added basic c# program to run my euler problem solutions. Added an EulerSolver class to contain methods for solving each of the euler problems, which are called and their solutions made into strings for display in console.

---
+++ euler-oiler/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace euler_oiler
+{
+    public class Program
+    {
+        static void Main(string[] Args)
+        {
+            ProduceOutputs();
+        }
+
+        private static void ProduceOutputs()
+        {
+            var EulerSolver = new EulerSolver();
+
+            Console.WriteLine(String.Format("1. The sum of all multiples of 3 and 5 that are less than 1000 is {0}", EulerSolver.SolveProblemOne()));
+        }
+    }
+}
